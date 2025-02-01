75bf29c7b2c7c7e7859e4b66b7374e98700a4a1a
YoussefAshour <youssefashour19@gmail.com>
2025-02-01 17:29:15 +0200

    Updated home page

--- src/Components/HomeGridProductsSection/HomeGridProductsSection.jsx
+++ src/Components/HomeGridProductsSection/HomeGridProductsSection.jsx
@@ -9,19 +9,19 @@ export default function HomeGridProductsSection() {
         {
           id: 1,
           name: "Bose Sport Earbuds -Wireless Earphones -Bluetooth In Ear...",
-          price: 1500,
+          price: `15,000`,
           image: "/src/assets/FlashSale1.png",
         },
         {
           id: 2,
           name: "Simple Mobile 4G LTE Prepaid Smartphone",
-          price: 1500,
+          price: `15,000`,
           image: "/src/assets/FlashSale2.png",
         },
         {
           id: 3,
           name: "4K UHD LED Smart TV with Chromecast Built-in",
-          price: 1500,
+          price: `15,000`,
           image: "/src/assets/FlashSale3.png",
         },
       ],
@@ -32,19 +32,19 @@ export default function HomeGridProductsSection() {
         {
           id: 1,
           name: "Samsung Electronics Samsung Galexy S21 5G",
-          price: 1500,
+          price: `15,000`,
           image: "/src/assets/BestSellers1.png",
         },
         {
           id: 2,
           name: "Simple Mobile 5G LTE Galexy 12 Mini 512GB Gaming Phone",
-          price: 1500,
+          price: `15,000`,
           image: "/src/assets/BestSellers2.png",
         },
         {
           id: 3,
           name: "Sony DSCHX8 High Zoom Point & Shoot Camera",
-          price: 1500,
+          price: `15,000`,
           image: "/src/assets/BestSellers3.png",
         },
       ],
@@ -55,19 +55,19 @@ export default function HomeGridProductsSection() {
         {
           id: 1,
           name: "Portable Wshing Machine, 11lbs capacity Model 18NMF...",
-          price: 1500,
+          price: `15,000`,
           image: "/src/assets/TopRated1.png",
         },
         {
           id: 2,
           name: "Sony DSCHX8 High Zoom Point & Shoot Camera",
-          price: 1500,
+          price: `15,000`,
           image: "/src/assets/TopRated2.png",
         },
         {
           id: 3,
           name: "Dell Optiplex 7000x7480 All-in-One Computer Monitor",
-          price: 1500,
+          price: `15,000`,
           image: "/src/assets/TopRated3.png",
         },
       ],
@@ -78,19 +78,19 @@ export default function HomeGridProductsSection() {
         {
           id: 1,
           name: "TOZO T6 True Wireless Earbuds Bluetooth Headpho...",
-          price: 1500,
+          price: `15,000`,
           image: "/src/assets/NewArrival1.png",
         },
         {
           id: 2,
           name: "JBL FLIP 4 -Waterproof Portable Bluetooth Speaker...",
-          price: 1500,
+          price: `15,000`,
           image: "/src/assets/NewArrival2.png",
         },
         {
           id: 3,
           name: "Wyze Cam Pan v2 1080p Pan/Tlt/Zoom Wi-Fi Indoor Smar...",
-          price: 1500,
+          price: `15,000`,
           image: "/src/assets/NewArrival3.png",
         },
       ],
@@ -117,11 +117,11 @@ export default function HomeGridProductsSection() {
                             />
                         </div>
                         <div className="flex-1 min-w-0">
-                            <h3 className="text-sm font-medium text-gray-900 leading-tight mb-1 line-clamp-2">
+                            <h3 className="text-sm font-semibold heading-font text-gray-900 leading-tight mb-1 line-clamp-2">
                                 {product.name}
                             </h3>
                             <p className="text-lg font-bold text-blue-500 group-hover:text-blue-600 transition-colors duration-200">
-                                ${product.price.toLocaleString()}
+                                {product.price.toLocaleString()} EGP
                             </p>
                         </div>
                     </div>
